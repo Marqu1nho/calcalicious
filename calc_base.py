79cf2701c0249d04f55b6a9a4be4744d70bbf55d
Marco Perez <marperez@overstock.com>
2021-05-03 19:40:46 -0600

    initial commit - I'll get to the rest tomorrow!

---
+++ calc_base.py
@@ -0,0 +1,7 @@
+class add_sub():
+    '''
+    class to handle addeding and subtracting
+    '''
+    def __init__(self, arg):
+        super(add_sub, self).__init__()
+        self.curr_result = None
